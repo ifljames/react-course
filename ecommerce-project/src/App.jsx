f8bac7cda1ac698a88cad3be419e1de947bde11c
James <ifl.jamesandrada@gmail.com>
2026-02-23 15:46:03 +0800

    Update filenames to match components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router";
-import { HomePage } from "./pages/HomePage";
-import { CheckoutPage } from "./pages/CheckoutPage";
-import { OrdersPage } from "./pages/OrdersPage";
-import { TrackingPage } from "./pages/TrackingPage";
+import { HomePage } from "./pages/homepage/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/tracking/TrackingPage";
 import "./App.css";
 
 function App() {
