30861c879a384f3990129c45499dcddaaf707839
James <ifl.jamesandrada@gmail.com>
2026-02-24 13:55:30 +0800

    Separate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
-import { HomePage } from "./pages/homepage/HomePage";
+import { HomePage } from "./pages/home/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/tracking/TrackingPage";
